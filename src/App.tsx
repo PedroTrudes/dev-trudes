f72fb899aa5dcb6086a2f48e87151f66b3c75181
PedroTrudes <pedro.jtrudes@gmail.com>
2026-04-15 17:40:49 -0300

    :bug: fix: created vercel.json

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,6 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
           <Route path="/login" element={<Login /> } />
           <Route path="/dev/contact" element={<Contacts />} />
